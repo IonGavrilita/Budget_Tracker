562b67e1813bd0ce0dc5f201d1ca5f712f33467f
Ion Gavrilita <gavrilita.ion.93@gmail.com>
2021-02-21 19:54:19 -0800

    updated service worker, db.js, package.json, added screenshot offline and online functionality

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,10 +5,6 @@ const FILES_TO_CACHE = [
   "/styles.css",
   "/db.js",
   "/manifest.json",
-  
-    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
-    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-
   ];
   
   const STATIC_CACHE = "static-cache-v1";
@@ -45,32 +41,33 @@ const FILES_TO_CACHE = [
         .then(() => self.clients.claim())
     );
   });
-  
-  self.addEventListener("fetch", event => {
-    // non GET requests are not cached and requests to other origins are not cached
-    if (
-      event.request.method !== "GET" ||
-      !event.request.url.startsWith(self.location.origin)
-    ) {
-      event.respondWith(fetch(event.request));
-      return;
-    }
-  
-    // handle runtime GET requests for data from /api routes
-    if (event.request.url.includes("/api/images")) {
-      // make network request and fallback to cache if network request fails (offline)
-      event.respondWith(
-        caches.open(RUNTIME_CACHE).then(cache => {
+   
+// fetch
+self.addEventListener("fetch", function (event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(RUNTIME_CACHE)
+        .then((cache) => {
           return fetch(event.request)
-            .then(response => {
-              cache.put(event.request, response.clone());
+            .then((response) => {
+              // If the response 200 store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+
               return response;
             })
-            .catch(() => caches.match(event.request));
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
         })
-      );
-      return;
-    }
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
   
     // use cache first for all other requests for performance
     event.respondWith(
